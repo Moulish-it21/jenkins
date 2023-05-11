270cd6911006452196fc9a2ba97b59553b9b0aed
Moulish-it21 <95067084+Moulish-it21@users.noreply.github.com>
2023-05-11 09:59:52 +0530

    Add files via upload

---
+++ Moulish.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the number 1 :"))
+b=int(input("Enter the number 2 :"))
+
+if(a<b):
+      print("A is Lesser than B")
+elif(a==b):
+     print("A is Equal to B")
+else:
+    print("B is greater than A")
